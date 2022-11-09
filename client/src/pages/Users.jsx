9bb63f9504da5273b7ffa801ddc61a9729dbd9ef
rollergod <renat.apaev@yandex.ru>
2022-11-09 11:29:42 +0300

    after refactoring with RTK

--- client/src/pages/Users.jsx
+++ client/src/pages/Users.jsx
@@ -5,44 +5,28 @@ import { Navigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { setUserName, getCurrentUser, getUsersAsync } from '../redux/slices/userSlice';
 
+import { useGetUsersQuery } from '../redux/slices/userApi';
+
 
 const Users = () => {
 
     const dispatch = useDispatch();
-    const username = useSelector(state => state.userSlice.username);
-    const users = useSelector(state => state.userSlice.users);
+    //const username = useSelector(state => state.userSlice.username);
+    // const users = useSelector(state => state.userSlice.users);
     const [redirectTo, setRedirectTo] = React.useState(false);
 
-    const getUser = () => {
-        try {
-            dispatch(getCurrentUser());
-
-        } catch (error) {
-            console.log(error, 'ERROR')
-            alert('Ошибка при получении данных')
-        }
-    }
-
-    const getUsers = () => {
-        try {
-            dispatch(getUsersAsync());
-
-        } catch (error) {
-            console.log(error, 'ERROR')
-            alert('Ошибка при получении данных')
-        }
-    }
+    const { data = [], isLoading } = useGetUsersQuery();
 
     React.useEffect(() => {
-        getUser();
-        getUsers();
+        // getUser();
+        // getUsers();
 
     }, []);
 
     return (
         <div class="parent">
             {
-                users.map((obj) => <UserItem key={obj.id} {...obj} />)
+                data.map((obj) => <UserItem key={obj.id} {...obj} />)
             }
         </div>
     )
