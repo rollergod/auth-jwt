ec8abe1d4274979b7f66dcd676088795813fa9cc
rollergod <renat.apaev@yandex.ru>
2022-11-09 10:11:06 +0300

    commit before refactoring

--- client/src/services/AuthService.js
+++ client/src/services/AuthService.js
@@ -2,18 +2,10 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setUserName } from "../redux/slices/userSlice";
 
-
-
-// export function AuthService() {
-//     const username = useSelector(state => state.userSlice.username);
-
-//     return username === '' ? false : username;
-// }
-
 export const AuthService = () => {
 
     const dispatch = useDispatch();
-
+    const error = '';
     const login = async (name, password) => {
         console.log('login method');
 
@@ -25,14 +17,16 @@ export const AuthService = () => {
             body: JSON.stringify({
                 name, password
             })
-        }).then((response) => {
+        }).then(async (response) => {
+
             if (!response.ok)
                 alert('Введены некорректные данные');
+
+            const { token } = await response.json();
+            sessionStorage.setItem('jwt', token);
+            dispatch(setUserName(name));
         }).catch(console.error);
 
-        const { token } = await response.json();
-        sessionStorage.setItem('jwt', token);
-        dispatch(setUserName(name));
     };
 
     const register = async (name, email, password) => {
